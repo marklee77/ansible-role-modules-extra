ec9c8426e0595fe99a3f14e8d0d2e2c180bd650b
Mark Stillwell <marklee@fortawesome.org>
2014-07-06 11:02:19 +0100

    password hash function...

--- filter_plugins/password_hash.py
+++ filter_plugins/password_hash.py
@@ -6,6 +6,14 @@ def ssha_password_hash(*a, **kw):
     pw_hash = sha1(password + salt).digest()
     return '{SSHA}' + b64encode("{}{}".format(pw_hash, salt))
 
+
+def sha512_password_hash(*a, **kw):
+    from crypt import crypt
+    password = a[0]
+    salt = a[1]
+    return crypt(password, '$6${}$'.format(salt))
+
+
 class FilterModule(object):
     ''' utility filter hash passwords '''
 
